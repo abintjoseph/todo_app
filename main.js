b92891e0309126a0715fd0cba0a7c09c073c9a08
Joseph <abintjoseph2018@gmail.com>
2024-10-07 23:13:26 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const inputbox = document.getElementById("inputbox");
+const listcontainer = document.getElementById("list-container");
+
+
+
+
+function addtask()
+{
+    if(inputbox.value =="")
+    {
+        alert('You mus write something');
+
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerText=inputbox.value;
+       listcontainer.appendChild(li);
+       let span = document.createElement('span');
+       span.innerHTML = "\u00d7";
+       li.appendChild(span);
+       }   
+       inputbox.value="";
+       savedata();
+ 
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI")
+    {
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName==="SPAN")
+    {
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false)
+
+
+function savedata()
+{
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+function showTask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
